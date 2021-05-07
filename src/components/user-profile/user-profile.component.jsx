8c86699d2cca1a7a2f91cc8653e74e0240565a34
revanthkumaar <iamrevanthkumar@gmail.com>
2021-05-07 14:14:39 +0530

    asdasd
    adadsasd

--- src/components/user-profile/user-profile.component.jsx
+++ src/components/user-profile/user-profile.component.jsx
@@ -1,33 +1,24 @@
 import React from 'react';
 
-class UserProfile extends React.Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            posts : []
+//import higher order component
+
+import withData from '../../with-data';
+
+const UserProfile = ({data}) => (
+   
+        <div className='container'>
+    
+
+        Posts: {
+               data.map(post => <div className="post">
+                    <h1>{post.title}</h1>
+                    <p>{post.body}</p>
+                </div>)
         }
-    }
+        </div>
 
-    componentDidMount(){
-        fetch('https://jsonplaceholder.typicode.com/posts')
-        .then(response => response.json())
-        .then(data => this.setState({posts:data.slice(0,5)}))
-    }
+)
 
-    render(){
-        return(
-            <div className='container'>
-        
 
-            Posts: {
-                    this.state.posts.map(post => <div className="post">
-                        <h1>{post.title}</h1>
-                        <p>{post.body}</p>
-                    </div>)
-            }
-            </div>
-        );
-    }
-}
-
-export default UserProfile;
+//wrapping the component to HOC
+export default withData(UserProfile);
